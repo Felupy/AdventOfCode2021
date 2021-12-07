cdb564475ba3ad294a272b4252c1e7837b185a84
Fernando <felupy@gmail.com>
2021-12-07 10:53:13 +0100

    Added solutions for Day7

---
+++ Day7/reto1.py
@@ -0,0 +1,27 @@
+import os
+
+
+
+
+input_file = "input.txt"
+cwd = os.getcwd()
+input_file_path = cwd + "\\" + input_file
+print(input_file_path)
+f = open(input_file_path, "r")
+
+lines = f.readlines()
+
+crabs_fuel = [int(x) for x in lines[0].split(',')]
+
+min_value = min(crabs_fuel)
+max_value = max(crabs_fuel)
+
+fuel_needed_by_pos = []
+for i in range(min_value, max_value):
+    fuel_needed = 0
+    for value in crabs_fuel:
+        fuel_needed += abs(i-value)
+    fuel_needed_by_pos.append(fuel_needed)    
+
+min_fuel_needed = min(fuel_needed_by_pos)
+print("Min fuel needed: %d" % min_fuel_needed)
